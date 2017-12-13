1923823532c32895faf48e8ab847096d98ac3018
filip-badea <filip.badea@cnmv.ro>
2017-12-13 18:33:42 +0200

    Update VoiceLauncher.java

--- vr/src/main/java/VoiceLauncher.java
+++ vr/src/main/java/VoiceLauncher.java
@@ -16,9 +16,48 @@ public class VoiceLauncher {
         // Set path to the acoustic model.
         configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
         // Set path to the dictionary.
-        configuration.setDictionaryPath("PATH_TO_YOUR_.DIC_FILE");
+        configuration.setDictionaryPath("languageModel/4371.dic");
         // Set path to the language model.
-        configuration.setLanguageModelPath("PATH_TO_YOUR_.LM_FILE");
+        configuration.setLanguageModelPath("languageModel/4371.lm");
+
+        LiveSpeechRecognizer recognize = new LiveSpeechRecognizer(configuration);
+
+        //Start Recognition Process (The bool parameter clears the previous cache if true)
+        recognize.startRecognition(true);
+
+        //Create SpeechResult Object
+        SpeechResult result;
+
+        //Checking if recognizer has recognized the speech
+        while ((result = recognize.getResult()) != null) {
+
+            //Get the recognized speech
+            String command = result.getHypothesis();
+            String work = null;
+            Process proc = null;
+
+            if(command.equalsIgnoreCase("open file manager")) {
+                //work = "explorer.exe";
+
+            } else if (command.equalsIgnoreCase("close file manager")) {
+                work = "explorer.exe";
+            } else if (command.equalsIgnoreCase("open browser")) {
+                try {
+                    proc = Runtime.getRuntime().exec("\"/Program Files (x86)/Google/Chrome/Application/chrome.exe\"");
+                    proc.waitFor();
+                    System.out.println("Google Chrome launched!");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (command.equalsIgnoreCase("close browser")) {
+                proc.destroy();
+            }
+            //In case command recognized is none of the above hence work might be null
+            if(work != null) {
+                //Execute the command
+                proc = Runtime.getRuntime().exec(work);
+            }
+        }
     }
 
 }
