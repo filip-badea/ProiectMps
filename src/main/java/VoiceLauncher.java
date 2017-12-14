22312e2c7efc1205376d516ed1ac68ab26d4ceda
badeaf <filip.badea@simplity.eu>
2017-12-14 17:44:43 +0200

    try to kill process

--- src/main/java/VoiceLauncher.java
+++ src/main/java/VoiceLauncher.java
@@ -9,6 +9,7 @@ import java.io.IOException;
  * @author ex094
  */
 public class VoiceLauncher {
+    private static Process explorerClosing;
     public static void main(String[] args) throws IOException {
         // Configuration Object
         Configuration configuration = new Configuration();
@@ -28,35 +29,57 @@ public class VoiceLauncher {
         //Create SpeechResult Object
         SpeechResult result;
 
+        //Process proc = null;
+        ProcessBuilder processExplorer = new ProcessBuilder("explorer.exe");
+        ProcessBuilder processExplorerExit = new ProcessBuilder("kill explorer.exe");
+        ProcessBuilder processBrowser = new ProcessBuilder("\"/Program Files (x86)/Google/Chrome/Application/chrome.exe\"");
+        Process explorer = null;
+        Process browser = null;
+        Process proc = null;
+
         //Checking if recognizer has recognized the speech
         while ((result = recognize.getResult()) != null) {
 
             //Get the recognized speech
             String command = result.getHypothesis();
             String work = null;
-            Process proc = null;
+
 
             if(command.equalsIgnoreCase("open file manager")) {
-                //work = "explorer.exe";
-
-            } else if (command.equalsIgnoreCase("close file manager")) {
-                work = "explorer.exe";
-            } else if (command.equalsIgnoreCase("open browser")) {
+                    explorer = processExplorer.start();
+                    System.out.println("Explorer launched!");
+            } else if (command.equalsIgnoreCase("close file manager"))  {
                 try {
-                    proc = Runtime.getRuntime().exec("\"/Program Files (x86)/Google/Chrome/Application/chrome.exe\"");
-                    proc.waitFor();
+                    explorer.destroyForcibly();
+                    System.out.println("Closing explorer");
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            else if (command.equalsIgnoreCase("open browser")) {
+                try {
+                    browser = Runtime.getRuntime().exec("\"/Program Files (x86)/Google/Chrome/Application/chrome.exe\"");
+                    browser.waitFor();
                     System.out.println("Google Chrome launched!");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             } else if (command.equalsIgnoreCase("close browser")) {
-                //proc.destroy();
+                try {
+                    proc =  Runtime.getRuntime().exec("taskkill /F /IM explorer.exe");
+                    proc.waitFor();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
             //In case command recognized is none of the above hence work might be null
-            if(work != null) {
+            /*if(work != null) {
                 //Execute the command
                 proc = Runtime.getRuntime().exec(work);
-            }
+            }*/
         }
     }
 
