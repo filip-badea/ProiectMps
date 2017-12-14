6e0913a696a1b2cd89a58a0bf44fda0346316eb6
badeaf <filip.badea@simplity.eu>
2017-12-14 16:31:23 +0200

    first

---
+++ src/main/java/VoiceLauncher.java
@@ -0,0 +1,63 @@
+//Imports
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import java.io.IOException;
+
+/**
+ *
+ * @author ex094
+ */
+public class VoiceLauncher {
+    public static void main(String[] args) throws IOException {
+        // Configuration Object
+        Configuration configuration = new Configuration();
+
+        // Set path to the acoustic model.
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        // Set path to the dictionary.
+        configuration.setDictionaryPath("languageModel/4371.dic");
+        // Set path to the language model.
+        configuration.setLanguageModelPath("languageModel/4371.lm");
+
+        LiveSpeechRecognizer recognize = new LiveSpeechRecognizer(configuration);
+
+        //Start Recognition Process (The bool parameter clears the previous cache if true)
+        recognize.startRecognition(true);
+
+        //Create SpeechResult Object
+        SpeechResult result;
+
+        //Checking if recognizer has recognized the speech
+        while ((result = recognize.getResult()) != null) {
+
+            //Get the recognized speech
+            String command = result.getHypothesis();
+            String work = null;
+            Process proc = null;
+
+            if(command.equalsIgnoreCase("open file manager")) {
+                //work = "explorer.exe";
+
+            } else if (command.equalsIgnoreCase("close file manager")) {
+                work = "explorer.exe";
+            } else if (command.equalsIgnoreCase("open browser")) {
+                try {
+                    proc = Runtime.getRuntime().exec("\"/Program Files (x86)/Google/Chrome/Application/chrome.exe\"");
+                    proc.waitFor();
+                    System.out.println("Google Chrome launched!");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (command.equalsIgnoreCase("close browser")) {
+                proc.destroy();
+            }
+            //In case command recognized is none of the above hence work might be null
+            if(work != null) {
+                //Execute the command
+                proc = Runtime.getRuntime().exec(work);
+            }
+        }
+    }
+
+}
